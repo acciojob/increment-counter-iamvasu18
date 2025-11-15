c660c7abfb8ff206dd11cdaca11455bf4f1316de
Dharavath Vasu Naik <168115062+iamvasu18@users.noreply.github.com>
2025-11-15 15:52:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let para=document.getElementById("counter");
+let count=0;
+let button=document.getElementById("incrementBtn");
+button.addEventListener("click",function(){
+	count+=1;
+	para.innerText=count;
+})
