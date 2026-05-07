9140145eaadaccfd70a441d7360a0f3ef89e5503
Dharavath Vasu Naik <168115062+iamvasu18@users.noreply.github.com>
2026-05-07 11:39:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let count=0;
+let display=document.getElementById("counter");
+let button=document.getElementById("incrementBtn");
+button.onclick=()=>{
+display.count++;
+}
